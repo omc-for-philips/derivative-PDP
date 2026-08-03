c9bac709dd682d5020ce7bb7c9dfa4ad91f72965
omnicom-for-philips <drelmalfec@gmail.com>
2026-08-03 18:30:44 +0100

    Add files via upload

---
+++ assets/submissionFeedbackMetrics-CRE1NZ0y.js
@@ -0,0 +1,21 @@
+import{c as s,P as a,an as i}from"./index-A8ogVlU0.js";import{M as t,B as n,S as c,A as l}from"./square-plus-DtejQ8kH.js";import{I as o,L as r}from"./layers-Bzdw2YnT.js";import{C as d}from"./trash-2-C5IKp7gr.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]],m=s("copy",u);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49",key:"ct8e1f"}],["path",{d:"M14.084 14.158a3 3 0 0 1-4.242-4.242",key:"151rxh"}],["path",{d:"M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143",key:"13bj9a"}],["path",{d:"m2 2 20 20",key:"1ooewy"}]],C=s("eye-off",h);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]],y=s("save",p);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["circle",{cx:"6",cy:"6",r:"3",key:"1lh9wr"}],["path",{d:"M8.12 8.12 12 12",key:"1alkpv"}],["path",{d:"M20 4 8.12 15.88",key:"xgtan2"}],["circle",{cx:"6",cy:"18",r:"3",key:"fqmcym"}],["path",{d:"M14.8 14.8 20 20",key:"ptml3r"}]],v=s("scissors",x);function j(e){return[{icon:a.jsx(i,{className:"size-4","aria-hidden":!0}),label:"Text changes",value:e.textChangeCount},{icon:a.jsx(o,{className:"size-4","aria-hidden":!0}),label:"Image briefs",value:e.imageBriefCount},{icon:a.jsx(r,{className:"size-4","aria-hidden":!0}),label:"New sections",value:e.newSectionCount},{icon:a.jsx(n,{className:"size-4","aria-hidden":!0}),label:"New features",value:e.newFeatureCount},{icon:a.jsx(c,{className:"size-4","aria-hidden":!0}),label:"New items",value:e.newItemCount},{icon:a.jsx(v,{className:"size-4","aria-hidden":!0}),label:"Sections removed",value:e.removedSectionCount},{icon:a.jsx(l,{className:"size-4","aria-hidden":!0}),label:"Moved sections",value:e.movedSectionCount},{icon:a.jsx(t,{className:"size-4","aria-hidden":!0}),label:"Image notes",value:e.imageNoteCount}]}function M(e){return[{icon:a.jsx(t,{className:"size-4","aria-hidden":!0}),label:"Artwork notes",value:e.noteCount},{icon:a.jsx(i,{className:"size-4","aria-hidden":!0}),label:"Pages marked",value:e.pageCount},{icon:a.jsx(o,{className:"size-4","aria-hidden":!0}),label:"Visual refs",value:e.visualReferenceCount},{icon:a.jsx(d,{className:"size-4","aria-hidden":!0}),label:"Specification notes",value:e.specificationCount}]}export{m as C,C as E,y as S,M as a,j as b};
